674155f0b6ebbcb58f295dbba1bd15b04a489071
kkishan1230 <kkishan1230@gmail.com>
2022-10-10 15:05:50 +0530

    netlify package added

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
